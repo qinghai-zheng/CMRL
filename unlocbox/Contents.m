c96f4510a782679377c029f25a9be74267e7ec0d
qinghai-zheng <69707026+qinghai-zheng@users.noreply.github.com>
2022-09-18 10:29:26 +0800

    Add files via upload

---
+++ unlocbox/Contents.m
@@ -0,0 +1,29 @@
+%  Unlocbox - About this toolbox
+%
+%  Starting the toolbox
+%    init_unlocbox      -  Initialize the UNLocBoX
+%    close_unlocbox     -  Stop the UNLocBoX
+%
+%
+%  For help, bug reports, suggestions etc. please send email to
+%  unlocbox-help@lists.sourceforge.net
+%
+%
+%   Url: http://lts2research.epfl.ch/unlocbox/doc/Contents.php
+
+% Copyright (C) 2012-2013 Nathanael Perraudin.
+% This file is part of UNLOCBOX version 1.5.0
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
